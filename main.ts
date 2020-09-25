cd585c542802e3d85d9565afa04d4c6c7e4678ce
sam4142 <71872261+sam4142@users.noreply.github.com>
2020-09-25 17:49:47 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onGesture(Gesture.Shake, function () {
+    if (randint(1, 6) == 6) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
